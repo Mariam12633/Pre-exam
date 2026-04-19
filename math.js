6f584ac6eb056eabedf33dcdad8236b91f15ce5b
Mariam12633 <cheddadimariam43@gmail.com>
2026-04-19 19:38:08 +0100

    ajouter les fichiers

---
+++ math.js
@@ -0,0 +1,6 @@
+console.log("Hello Git 🚀");
+
+import { add } from "./math.js";
+
+const result = add(2, 3);
+console.log("Result:", result);
---
+++ utils.js
@@ -0,0 +1,7 @@
+export function greet(name) {
+    return `Hello, ${name}! 👋`;
+}
+
+export function isEven(number) {
+    return number % 2 === 0;
+}
